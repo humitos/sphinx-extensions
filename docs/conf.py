3e0789e9b267dfaf7608306338583f087f936a6c
Matthias Geier <Matthias.Geier@gmail.com>
2023-10-14 16:27:21 +0200

    Replace ipython_kernel_config.py with matplotlibrc

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,6 @@ autosectionlabel_prefix_document = True
 nbsphinx_execute = 'auto'
 nbsphinx_execute_arguments = [
     "--InlineBackend.figure_formats={'svg', 'pdf'}",
-    "--InlineBackend.rc=figure.dpi=96",
 ]
 
 autoapi_dirs = [
